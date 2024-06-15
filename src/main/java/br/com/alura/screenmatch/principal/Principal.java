b93d6c2b7f1cc5f72e9e4f5e59f7dc66c4e0673e
jvictornascimento <victojoao789@gmail.com>
2024-06-15 10:47:17 -0300

    created loop for menu right

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -21,16 +21,21 @@ public class Principal {
     private final String ENDERECO = "https://www.omdbapi.com/?t=";
     private final String API_KEY = "&apikey=6585022c";
 
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
+
     public void exibeMenu() {
+        var opcao = -1;
+        while (opcao != 0 ){
         var menu = """
                 1 - Buscar séries
                 2 - Buscar episódios
+                3 - Listar séries Buscadas
                 
                 0 - Sair                                 
                 """;
 
         System.out.println(menu);
-        var opcao = leitura.nextInt();
+        opcao = leitura.nextInt();
         leitura.nextLine();
 
         switch (opcao) {
@@ -40,16 +45,21 @@ public class Principal {
             case 2:
                 buscarEpisodioPorSerie();
                 break;
+            case 3:
+               ListarSeriesBuscadas();
+                break;
             case 0:
                 System.out.println("Saindo...");
                 break;
             default:
                 System.out.println("Opção inválida");
         }
+        }
     }
 
     private void buscarSerieWeb() {
         DadosSerie dados = getDadosSerie();
+        dadosSeries.add(dados);
         System.out.println(dados);
     }
 
@@ -71,5 +81,9 @@ public class Principal {
             temporadas.add(dadosTemporada);
         }
         temporadas.forEach(System.out::println);
+    }
+
+    private void  ListarSeriesBuscadas(){
+        dadosSeries.forEach(System.out::println);
     }
 }
