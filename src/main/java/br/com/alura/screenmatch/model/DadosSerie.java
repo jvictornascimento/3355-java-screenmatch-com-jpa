39c97c083db9248b82533b68d398a5cb72244ec6
jvictornascimento <victojoao789@gmail.com>
2024-06-15 10:45:33 -0300

    Revert "created loop for menu"
    This reverts commit 1189033b0b7364e8a2607b343874cad3599ef39b.

--- src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosSerie.java
@@ -6,9 +6,5 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DadosSerie(@JsonAlias("Title") String titulo,
                          @JsonAlias("totalSeasons") Integer totalTemporadas,
-                         @JsonAlias("imdbRating") String avaliacao,
-                         @JsonAlias("Genre") String genero,
-                         @JsonAlias("Actors") String atores,
-                         @JsonAlias("Poster") String imagemDeCapa,
-                         @JsonAlias("Plot") String sinopse){
+                         @JsonAlias("imdbRating") String avaliacao) {
 }
