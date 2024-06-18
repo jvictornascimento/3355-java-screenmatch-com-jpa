3ccdfe967c6922af6fdbcca3a180bb1a27e3c490
jvictornascimento <victojoao789@gmail.com>
2024-06-17 23:13:12 -0300

    configuration serie class for entity

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,15 +1,23 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
+import jakarta.persistence.*;
 
 import java.util.Objects;
 import java.util.OptionalDouble;
+@Entity
+@Table(name = "series")
 
 public class Serie {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
     private String titulo;
     private Integer totalTemporadas;
     private Double avaliacao;
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
     private String atores;
     private String imagemDeCapa;
@@ -24,6 +32,18 @@ public class Serie {
         this.atores = dadosSerie.atores();
         this.imagemDeCapa = dadosSerie.imagemDeCapa();
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse().trim());
+    }
+
+    public Serie() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
